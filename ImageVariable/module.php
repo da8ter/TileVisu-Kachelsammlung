d6961d31597428e20453fe1b0e59dbbd259dd604
da8ter <58776097+da8ter@users.noreply.github.com>
2024-01-16 15:38:29 +0100

    ads

--- ImageVariable/module.php
+++ ImageVariable/module.php
@@ -1,134 +1,143 @@
 <?php
-    class TileVisuImageVariable extends IPSModule
+class TileVisuImageVariable extends IPSModule
+{
+    public function Create()
     {
-        
-        public function Create() {
-            //Never delete this line!
-            parent::Create();
+        // Nie diese Zeile löschen!
+        parent::Create();
 
-            // Drei Eigenschaften für die dargestellten Zähler
+        // Drei Eigenschaften für die dargestellten Zähler
+        $this->RegisterPropertyInteger("bgImage", 0);
+        $this->RegisterPropertyInteger("Variable", 0);
+        $this->RegisterPropertyFloat('Schriftgroesse', 1);
+        $this->RegisterPropertyFloat('Bildtransparenz', 1);
+        $this->RegisterPropertyInteger('Kachelhintergrundfarbe', 0xFFFFFF);
 
-            $this->RegisterPropertyInteger("bgImage", 0);
-            $this->RegisterPropertyInteger("Variable", 0);
-            $this->RegisterPropertyFloat('Schriftgroesse', 1);
-            $this->RegisterPropertyFloat('Bildtransparenz', 1);
-            $this->RegisterPropertyInteger('Kachelhintergrundfarbe', 0xFFFFFF);
-            // Visualisierungstyp auf 1 setzen, da wir HTML anbieten möchten
-            $this->SetVisualizationType(1);
+        // Visualisierungstyp auf 1 setzen, da wir HTML anbieten möchten
+        $this->SetVisualizationType(1);
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        // Aktualisiere registrierte Nachrichten
+        foreach ($this->GetMessageList() as $senderID => $messageIDs)
+        {
+            foreach ($messageIDs as $messageID)
+            {
+                $this->UnregisterMessage($senderID, $messageID);
+            }
         }
 
-        public function ApplyChanges() {
-            parent::ApplyChanges();
-            // Aktualisiere registrierte Nachrichten
-            foreach ($this->GetMessageList() as $senderID => $messageIDs) {
-                foreach($messageIDs as $messageID) {
-                    $this->UnregisterMessage($senderID, $messageID);
+        $variableProperties = ['bgImage', 'Variable'];
+        foreach ($variableProperties as $VariableProperty)
+        {
+            $this->RegisterMessage($this->ReadPropertyInteger($VariableProperty), VM_UPDATE);
+        }
+
+        // Schicke eine komplette Update-Nachricht an die Darstellung, da sich ja Parameter geändert haben können
+        $this->UpdateVisualizationValue($this->GetFullUpdateMessage());
+    }
+
+    public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+    {
+        $variableProperties = ['bgImage', 'Variable'];
+        foreach ($variableProperties as $index => $VariableProperty)
+        {
+            if ($SenderID === $this->ReadPropertyInteger($VariableProperty))
+            {
+                switch ($Message)
+                {
+                    case VM_UPDATE:
+                        // Teile der HTML-Darstellung den neuen Wert mit. Damit dieser korrekt formatiert ist, holen wir uns den von der Variablen via GetValueFormatted
+                        $this->UpdateVisualizationValue(json_encode(['variable' . ($index + 1) => GetValueFormatted($this->ReadPropertyInteger($VariableProperty))]));
+                        break;
                 }
             }
-
-            foreach(['bgImage', 'Variable'] as $VariableProperty) {
-                $this->RegisterMessage($this->ReadPropertyInteger($VariableProperty), VM_UPDATE);
-            }
-
-            // Schicke eine komplette Update-Nachricht an die Darstellung, da sich ja Parameter geändert haben können
-            $this->UpdateVisualizationValue($this->GetFullUpdateMessage());
         }
+    }
 
-        public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
-            foreach(['bgImage', 'Variable'] as $index => $VariableProperty) {
-                if ($SenderID === $this->ReadPropertyInteger($VariableProperty)) {
-                    switch ($Message) {
-                        case VM_UPDATE:
-                            // Teile der HTML-Darstellung den neuen Wert mit. Damit dieser korrekt formatiert ist, holen wir uns den von der Variablen via GetValueFormatted
-                            $this->UpdateVisualizationValue(json_encode(['variable' . ($index + 1) => GetValueFormatted($this->ReadPropertyInteger($VariableProperty))]));
+    public function GetVisualizationTile()
+    {
+        // Füge ein Skript hinzu, um beim Laden, analog zu Änderungen bei Laufzeit, die Werte zu setzen
+        $initialHandling = '<script>handleMessage(' . json_encode($this->GetFullUpdateMessage()) . ')</script>';
+
+        // Füge statisches HTML aus Datei hinzu
+        $module = file_get_contents(__DIR__ . '/module.html');
+
+        // Gebe alles zurück.
+        // Wichtig: $initialHandling nach hinten, da die Funktion handleMessage erst im HTML definiert wird
+        return $module . $initialHandling;
+    }
+
+    // Generiere eine Nachricht, die alle Elemente in der HTML-Darstellung aktualisiert
+    private function GetFullUpdateMessage()
+    {
+        $VariableID = $this->ReadPropertyInteger('Variable');
+        $VariableExists = IPS_VariableExists($VariableID);
+        $result = [
+            'Variable' => $VariableExists
+        ];
+        $result['fontsize'] = $this->ReadPropertyFloat('Schriftgroesse');
+        $result['hintergrundfarbe'] = '#' . sprintf('%06X', $this->ReadPropertyInteger('Kachelhintergrundfarbe'));
+        $result['transparenz'] = $this->ReadPropertyFloat('Bildtransparenz');
+        if ($VariableExists)
+        {
+            $result['variable1'] = GetValueFormatted($VariableID);
+
+            // Prüfe vorweg, ob ein Bild ausgewählt wurde
+            $imageID = $this->ReadPropertyInteger('bgImage');
+            if (IPS_MediaExists($imageID))
+            {
+                $image = IPS_GetMedia($imageID);
+                if ($image['MediaType'] === MEDIATYPE_IMAGE)
+                {
+                    $imageFile = explode('.', $image['MediaFile']);
+                    $imageContent = '';
+                    // Falls ja, ermittle den Anfang der src basierend auf dem Dateitypen
+                    switch (end($imageFile))
+                    {
+                        case 'bmp':
+                            $imageContent = 'data:image/bmp;base64,';
+                            break;
+
+                        case 'jpg':
+                        case 'jpeg':
+                            $imageContent = 'data:image/jpeg;base64,';
+                            break;
+
+                        case 'gif':
+                            $imageContent = 'data:image/gif;base64,';
+                            break;
+
+                        case 'png':
+                            $imageContent = 'data:image/png;base64,';
+                            break;
+
+                        case 'ico':
+                            $imageContent = 'data:image/x-icon;base64,';
                             break;
                     }
+
+                    // Nur fortfahren, falls Inhalt gesetzt wurde. Ansonsten ist das Bild kein unterstützter Dateityp
+                    if ($imageContent)
+                    {
+                        // Hänge base64-codierten Inhalt des Bildes an
+                        $imageContent .= IPS_GetMediaContent($imageID);
+                        $result['image1'] = $imageContent;
+                    }
                 }
             }
-        }
-    
-        public function GetVisualizationTile() {
-            // Füge ein Skript hinzu, um beim laden, analog zu Änderungen bei Laufzeit, die Werte zu setzen
-            // Obwohl die Rückgabe von GetFullUpdateMessage ja schon JSON-codiert ist wird dennoch ein weiteres mal json_encode ausgeführt
-            // Damit wird dem String Anführungszeichen hinzugefügt und eventuelle Anführungszeichen innerhalb werden korrekt escaped
-            $initialHandling = '<script>handleMessage(' . json_encode($this->GetFullUpdateMessage()) . ');</script>';
-
-            // Füge statisches HTML aus Datei hinzu
-            $module = file_get_contents(__DIR__ . '/module.html');
-
-            // Gebe alles zurück. 
-            // Wichtig: $initialHandling nach hinten, da die Funktion handleMessage ja erst im HTML definiert wird
-            return $module . $initialHandling;
-        }
-
-            // Generiere eine Nachricht, die alle Elemente in der HTML-Darstellung aktualisiert
-            private function GetFullUpdateMessage() {
-                $result = []; // Initialisiere das Ergebnis-Array
-
-                $VariableID = $this->ReadPropertyInteger('Variable');
-                $VariableExists = IPS_VariableExists($VariableID);
-                $result = [
-                    'Variable' => $VariableExists
-                ];
-                $result['fontsize'] = $this->ReadPropertyFloat('Schriftgroesse');
-                $result['hintergrundfarbe'] = '#' . sprintf('%06X',$this->ReadPropertyInteger('Kachelhintergrundfarbe'));
-                $result['transparenz'] = $this->ReadPropertyFloat('Bildtransparenz');
-                if ($VariableExists) {
-                    $result['variable1'] = GetValueFormatted($VariableID);
-
-                    // Prüfe vorweg, ob ein Bild ausgewählt wurde
-                    $imageID = $this->ReadPropertyInteger('bgImage');
-                    if (IPS_MediaExists($imageID)) {
-                        $image = IPS_GetMedia($imageID);
-                        if ($image['MediaType'] === MEDIATYPE_IMAGE) {
-                            $imageFile = explode('.', $image['MediaFile']);
-                            $imageContent = '';
-                            // Falls ja, ermittle den Anfang der src basierend auf dem Dateitypen
-                            switch (end($imageFile)) {
-                                case 'bmp':
-                                    $imageContent = 'data:image/bmp;base64,';
-                                    break;
-            
-                                case 'jpg':
-                                case 'jpeg':
-                                    $imageContent = 'data:image/jpeg;base64,';
-                                    break;
-            
-                                case 'gif':
-                                    $imageContent = 'data:image/gif;base64,';
-                                    break;
-            
-                                case 'png':
-                                    $imageContent = 'data:image/png;base64,';
-                                    break;
-            
-                                case 'ico':
-                                    $imageContent = 'data:image/x-icon;base64,';
-                                    break;
-                            }
-
-                            // Nur fortfahren, falls Inhalt gesetzt wurde. Ansonsten ist das Bild kein unterstützter Dateityp
-                            if ($imageContent) {
-                                // Hänge base64-codierten Inhalt des Bildes an
-                                $imageContent .= IPS_GetMediaContent($imageID);
-                                $result['image1'] = $imageContent;
-                            }
-
-                        }
-                    }
-                    else{
-                        $imageContent = 'data:image/png;base64,';
-                        $imageContent .= base64_encode(file_get_contents(__DIR__ . '/assets/placeholder.png'));
-                        $result['image1'] = $imageContent;
-                    }                    
-                    
-                
-                
-                return json_encode($result);
-                
+            else
+            {
+                $imageContent = 'data:image/png;base64,';
+                $imageContent .= base64_encode(file_get_contents(__DIR__ . '/assets/placeholder.png'));
+                $result['image1'] = $imageContent;
             }
+        }
 
-    
+        return json_encode($result);
     }
 }
 ?>
