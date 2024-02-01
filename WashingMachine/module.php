261350bc581aadaf683495cfb7f7fd3208e27db1
da8ter <58776097+da8ter@users.noreply.github.com>
2024-02-01 08:34:02 +0100

    fg

--- WashingMachine/module.php
+++ WashingMachine/module.php
@@ -110,7 +110,7 @@ class TileVisuWashingMaschine extends IPSModule
             $result['status'] = $this->CheckAndGetValueFormatted('Status');
             $result['programm'] = $this->CheckAndGetValueFormatted('Programm');
             $result['programmfortschritt'] = $this->CheckAndGetValueFormatted('Programmfortschritt');
-            $result['programmfortschrittvalue'] = isset(GetValue($this->ReadPropertyInteger('Programmfortschritt')));
+            $result['programmfortschrittvalue'] = IPS_VariableExists($this->ReadPropertyInteger('Programmfortschritt')) ? GetValue($this->ReadPropertyInteger('Programmfortschritt')) : null;
             $result['restlaufzeit'] = $this->CheckAndGetValueFormatted('restlaufzeit');
             $result['verbrauch'] = $this->CheckAndGetValueFormatted('Verbrauch');
             $result['verbrauchtag'] = $this->CheckAndGetValueFormatted('VerbrauchTag');
