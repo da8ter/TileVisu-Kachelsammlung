ccc7492424de5b388f02f7a7ecc5cc4c06b5f96c
da8ter <58776097+da8ter@users.noreply.github.com>
2024-02-07 09:32:51 +0100

    dsf

--- WashingMachine/module.php
+++ WashingMachine/module.php
@@ -166,7 +166,7 @@ class TileVisuWashingMaschine extends IPSModule
 
 
 
-    public function UpdateList($id, $ident)
+    public function UpdateList()
     {
         $listData = []; // Hier sammeln Sie die Daten für Ihre Liste
     
