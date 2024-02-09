2836861deed76f68241cc72e7a79e5e16e44e68a
da8ter <58776097+da8ter@users.noreply.github.com>
2024-02-09 16:18:01 +0100

    1

---
+++ PV_Overview/module.php
@@ -0,0 +1,233 @@
+<?php
+class TileVisuWashingMaschine extends IPSModule
+{
+    public function Create()
+    {
+        // Nie diese Zeile löschen!
+        parent::Create();
+
+        $this->RegisterPropertyInteger("Produktion", 0);
+        $this->RegisterPropertyInteger("Eigenverbrauch", 0);
+        $this->RegisterPropertyInteger("Einspeisung", 0);
+        $this->RegisterPropertyInteger("Verbrauch", 0);
+        $this->RegisterPropertyInteger("Eigenproduktion", 0);
+        $this->RegisterPropertyInteger("Zukauf", 0);
+        $this->RegisterPropertyInteger("EigenverbrauchVerlaufFarbe1", 2674091);
+        $this->RegisterPropertyInteger("EigenverbrauchVerlaufFarbe2", 2132596);
+        $this->RegisterPropertyInteger("EigenproduktionVerlaufFarbe1", 2674091);
+        $this->RegisterPropertyInteger("EigenproduktionVerlaufFarbe2", 2132596);
+        //Kachellayout
+        $this->RegisterPropertyInteger("bgImage", 0);
+        $this->RegisterPropertyFloat('Bildtransparenz', 0.7);
+        $this->RegisterPropertyInteger('Kachelhintergrundfarbe', 0x000000);
+        $this->RegisterPropertyInteger('Schriftfarbe', 0xFFFFFF);
+        $this->RegisterPropertyFloat("Schriftgroesse", 1);
+        $this->RegisterPropertyInteger("EinspeisungFarbe", 2132596);
+        $this->RegisterPropertyInteger("ZukaufFarbe", 2132596);
+
+
+
+        // Visualisierungstyp auf 1 setzen, da wir HTML anbieten möchten
+        $this->SetVisualizationType(1);
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        // Aktualisiere registrierte Nachrichten
+        foreach ($this->GetMessageList() as $senderID => $messageIDs)
+        {
+            foreach ($messageIDs as $messageID)
+            {
+                $this->UnregisterMessage($senderID, $messageID);
+            }
+        }
+
+
+        foreach (['Produktion', 'Eigenverbrauch', 'Einspeisung', 'Verbrauch', 'Eigenproduktion', 'Zukauf'] as $VariableProperty)        {
+            $this->RegisterMessage($this->ReadPropertyInteger($VariableProperty), VM_UPDATE);
+        }
+
+        // Schicke eine komplette Update-Nachricht an die Darstellung, da sich ja Parameter geändert haben können
+        $this->UpdateVisualizationValue($this->GetFullUpdateMessage());
+    }
+
+    public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+    {
+
+        foreach (['Produktion', 'Eigenverbrauch', 'Einspeisung', 'Verbrauch', 'Eigenproduktion', 'Zukauf'] as $index => $VariableProperty)
+        {
+            if ($SenderID === $this->ReadPropertyInteger($VariableProperty))
+            {
+                
+
+                switch ($Message)
+                {
+                    case VM_UPDATE:
+                        
+                        // Teile der HTML-Darstellung den neuen Wert mit. Damit dieser korrekt formatiert ist, holen wir uns den von der Variablen via GetValueFormatted
+                        $this->UpdateVisualizationValue(json_encode([$VariableProperty => GetValueFormatted($this->ReadPropertyInteger($VariableProperty))]));
+                        $this->UpdateVisualizationValue(json_encode([$VariableProperty . 'Value' => GetValue($this->ReadPropertyInteger($VariableProperty))]));
+                        break; // Beende die Schleife, da der passende Wert gefunden wurde
+
+                }
+            }
+        }
+    }
+
+
+    public function RequestAction($Ident, $value) {
+        // Nachrichten von der HTML-Darstellung schicken immer den Ident passend zur Eigenschaft und im Wert die Differenz, welche auf die Variable gerechnet werden soll
+        $variableID = $this->ReadPropertyInteger($Ident);
+        if (!IPS_VariableExists($variableID)) {
+            $this->SendDebug('Error in RequestAction', 'Variable to be updated does not exist', 0);
+            return;
+        }
+            // Umschalten des Werts der Variable
+        $currentValue = GetValue($variableID);
+        //SetValue($variableID, !$currentValue);
+        RequestAction($variableID, !$currentValue);
+    }
+
+
+    public function GetVisualizationTile()
+    {
+        // Füge ein Skript hinzu, um beim Laden, analog zu Änderungen bei Laufzeit, die Werte zu setzen
+        $initialHandling = '<script>handleMessage(' . json_encode($this->GetFullUpdateMessage()) . ')</script>';
+
+        // Füge statisches HTML aus Datei hinzu
+        $module = file_get_contents(__DIR__ . '/module.html');
+
+        // Gebe alles zurück.
+        // Wichtig: $initialHandling nach hinten, da die Funktion handleMessage erst im HTML definiert wird
+        return $module . $initialHandling;
+    }
+
+
+
+    // Generiere eine Nachricht, die alle Elemente in der HTML-Darstellung aktualisiert
+    private function GetFullUpdateMessage() {
+
+        $result = [];
+             $result['produktion'] = IPS_VariableExists($this->ReadPropertyInteger('Produktion')) ? $this->CheckAndGetValueFormatted('Produktion') : null;
+            $result['produktionvalue'] = IPS_VariableExists($this->ReadPropertyInteger('Produktion')) ? GetValue($this->ReadPropertyInteger('Produktion')) : null;
+            $result['eigenverbrauch'] = IPS_VariableExists($this->ReadPropertyInteger('Eigenverbrauch')) ? $this->CheckAndGetValueFormatted('Eigenverbrauch') : null;
+            $result['eigenverbrauchvalue'] = IPS_VariableExists($this->ReadPropertyInteger('Eigenverbrauch')) ? GetValue($this->ReadPropertyInteger('Eigenverbrauch')) : null;
+            $result['verbrauch'] = IPS_VariableExists($this->ReadPropertyInteger('Verbrauch')) ? $this->CheckAndGetValueFormatted('Verbrauch') : null;
+            $result['verbrauchvalue'] = IPS_VariableExists($this->ReadPropertyInteger('Verbrauch')) ? GetValue($this->ReadPropertyInteger('Verbrauch')) : null;
+            $result['eigenproduktion'] = IPS_VariableExists($this->ReadPropertyInteger('Eigenproduktion')) ? $this->CheckAndGetValueFormatted('Eigenproduktion') : null;
+            $result['eigenproduktionvalue'] = IPS_VariableExists($this->ReadPropertyInteger('Eigenproduktion')) ? GetValue($this->ReadPropertyInteger('Eigenproduktion')) : null;
+            $result['zukauf'] = IPS_VariableExists($this->ReadPropertyInteger('Zukauf')) ? $this->CheckAndGetValueFormatted('Zukauf') : null;
+            $result['zukaufvalue'] = IPS_VariableExists($this->ReadPropertyInteger('Zukauf')) ? GetValue($this->ReadPropertyInteger('Zukauf')) : null;
+            $result['eigenverbrauchverlauffarbe1'] =  '#' . sprintf('%06X', $this->ReadPropertyInteger('EigenverbrauchVerlaufFarbe1'));
+            $result['eigenverbrauchverlauffarbe2'] =  '#' . sprintf('%06X', $this->ReadPropertyInteger('EigenverbrauchVerlaufFarbe2'));
+            $result['eigenproduktionverlauffarbe1'] =  '#' . sprintf('%06X', $this->ReadPropertyInteger('EigenproduktionVerlaufFarbe1'));
+            $result['eigenproduktionverlauffarbe2'] =  '#' . sprintf('%06X', $this->ReadPropertyInteger('EigenproduktionVerlaufFarbe2'));
+            $result['bildtransparenz'] =  $this->ReadPropertyFloat('Bildtransparenz');
+            $result['kachelhintergrundfarbe'] =  '#' . sprintf('%06X', $this->ReadPropertyInteger('Kachelhintergrundfarbe'));
+            $result['schriftfarbe'] =  '#' . sprintf('%06X', $this->ReadPropertyInteger('Schriftfarbe'));
+            $result['schriftgroesse'] =  $this->ReadPropertyFloat('Schriftgroesse');
+            $result['einspeisungfarbe'] =  '#' . sprintf('%06X', $this->ReadPropertyInteger('EinspeisungFarbe'));
+            $result['zukauffarbe'] =  '#' . sprintf('%06X', $this->ReadPropertyInteger('ZukaufFarbe'));
+        return json_encode($result);
+    }
+
+
+
+    private function CheckAndGetValueFormatted($property) {
+        $id = $this->ReadPropertyInteger($property);
+        if (IPS_VariableExists($id)) {
+            return GetValueFormatted($id);
+        }
+        return false;
+    }
+
+
+    private function GetColor($id) {
+        $variable = IPS_GetVariable($id);
+        $Value = GetValue($id);
+        $profile = $variable['VariableCustomProfile'] ?: $variable['VariableProfile'];
+
+        if ($profile && IPS_VariableProfileExists($profile)) {
+            $p = IPS_GetVariableProfile($profile);
+            
+            foreach ($p['Associations'] as $association) {
+                if (isset($association['Value'], $association['Color']) && $association['Value'] == $Value) {
+                    return $association['Color'] === -1 ? "" : sprintf('%06X', $association['Color']);
+                    
+                }
+            }
+        }
+        return "";
+    }
+
+
+    private function GetColorRGB($hexcolor) {
+        $transparenz = $this->ReadPropertyFloat('InfoMenueTransparenz');
+        if($hexcolor != "-1")
+        {
+                $hexColor = sprintf('%06X', $hexcolor);
+                // Prüft, ob der Hex-Farbwert gültig ist
+                if (strlen($hexColor) == 6) {
+                    $r = hexdec(substr($hexColor, 0, 2));
+                    $g = hexdec(substr($hexColor, 2, 2));
+                    $b = hexdec(substr($hexColor, 4, 2));
+                    return "rgba($r, $g, $b, $transparenz)";
+                } else {
+                    // Fallback für ungültige Eingaben
+                    return $hexColor;
+                }
+        }
+        else {
+            return "";
+        }
+    }
+
+    private function GetIcon($id, $varicon) {
+        $variable = IPS_GetVariable($id);
+        $Value = GetValue($id);
+        $icon = "";
+        //Abfragen ob das Variablen-Icon oder das Profil-Icon verwendet werden soll
+        if($varicon == true){
+        $icon = IPS_GetObject($id);
+            if($icon['ObjectIcon'] != ""){
+                $icon = $icon['ObjectIcon'];
+            }
+            else {
+                $icon = "Transparent";
+            }
+        }
+        else {
+        // Profil-Icon abrufen
+        $profile = $variable['VariableCustomProfile'] ?: $variable['VariableProfile'];
+        $icon = "";
+
+        if ($profile && IPS_VariableProfileExists($profile)) {
+            $p = IPS_GetVariableProfile($profile);
+
+            foreach ($p['Associations'] as $association) {
+                if (isset($association['Value']) && $association['Icon'] != "" && $association['Value'] == $Value) {
+                    $icon = $association['Icon'];
+                    break;
+                }
+            }
+
+            if ($icon == "" && isset($p['Icon']) && $p['Icon'] != "") {
+                $icon = $p['Icon'];
+            }
+
+            if ($icon == "") {
+                $icon = "Transparent";
+            }
+        }
+        else {
+            $icon = "Transparent";
+        }
+        
+        }
+        return $icon;
+    }
+
+}
+?>
