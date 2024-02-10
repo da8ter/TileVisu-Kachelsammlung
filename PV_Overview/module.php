8e82547766e836b5d9926a157eba0992720d7b6a
da8ter <58776097+da8ter@users.noreply.github.com>
2024-02-10 13:51:14 +0100

    sgf

--- PV_Overview/module.php
+++ PV_Overview/module.php
@@ -92,15 +92,17 @@ class TileVisuPVoverview extends IPSModule
                             }
                         }
             
-                        $verbrauchID = $this->ReadPropertyInteger('Verbrauch');
-                        $verbrauch = 1; // Standardwert setzen
+                        //$verbrauchID = $this->ReadPropertyInteger('Verbrauch');
+                        //$verbrauch = 1; // Standardwert setzen
                         
-                        if (IPS_VariableExists($verbrauchID) && AC_GetLoggingStatus($archivID, $verbrauchID)) {
-                            $verbrauch_heute_archiv = AC_GetAggregatedValues($archivID, $verbrauchID, 1 /* Täglich */, strtotime("today 00:00"), time(), 0);
-                            if (!empty($verbrauch_heute_archiv)) {
-                                $verbrauch = round($verbrauch_heute_archiv[0]['Avg'], 2);
-                            }
-                        }
+                        //if (IPS_VariableExists($verbrauchID) && AC_GetLoggingStatus($archivID, $verbrauchID)) {
+                         //   $verbrauch_heute_archiv = AC_GetAggregatedValues($archivID, $verbrauchID, 1 /* Täglich */, strtotime("today 00:00"), time(), 0);
+                         //   if (!empty($verbrauch_heute_archiv)) {
+                         //       $verbrauch = round($verbrauch_heute_archiv[0]['Avg'], 2);
+                         //   }
+                        //}
+
+
             
                         $exportID = $this->ReadPropertyInteger('Export');
                         $export = 1; // Standardwert setzen
@@ -115,9 +117,10 @@ class TileVisuPVoverview extends IPSModule
                         $export_prozent = round($export / $produktion * 100, 0);
                         $eigenverbrauch_prozent = round(100 - $export_prozent, 0);
                         $eigenverbrauch = round($produktion / 100 * $eigenverbrauch_prozent,2);
-                        $import_prozent = round($import / $verbrauch * 100, 0);
                         $eigenproduktion_prozent = round(100 - $import_prozent, 0);
                         $eigenproduktion = round($produktion - $export, 2);
+                        $verbrauch = $import + $eigenproduktion;
+                        $import_prozent = round($import / $verbrauch * 100, 0);
 
                         $this->UpdateVisualizationValue(json_encode(['produktion' => $produktion]));
                         $this->UpdateVisualizationValue(json_encode(['import' => $import]));
@@ -221,15 +224,15 @@ class TileVisuPVoverview extends IPSModule
                 }
             }
 
-            $verbrauchID = $this->ReadPropertyInteger('Verbrauch');
-            $verbrauch = 1; // Standardwert setzen
+           // $verbrauchID = $this->ReadPropertyInteger('Verbrauch');
+          //  $verbrauch = 1; // Standardwert setzen
             
-            if (IPS_VariableExists($verbrauchID) && AC_GetLoggingStatus($archivID, $verbrauchID)) {
-                $verbrauch_heute_archiv = AC_GetAggregatedValues($archivID, $verbrauchID, 1 /* Täglich */, strtotime("today 00:00"), time(), 0);
-                if (!empty($verbrauch_heute_archiv)) {
-                    $verbrauch = round($verbrauch_heute_archiv[0]['Avg'], 2);
-                }
-            }
+          //  if (IPS_VariableExists($verbrauchID) && AC_GetLoggingStatus($archivID, $verbrauchID)) {
+           //     $verbrauch_heute_archiv = AC_GetAggregatedValues($archivID, $verbrauchID, 1 /* Täglich */, strtotime("today 00:00"), time(), 0);
+           //     if (!empty($verbrauch_heute_archiv)) {
+           //         $verbrauch = round($verbrauch_heute_archiv[0]['Avg'], 2);
+           //     }
+           // }
 
             $exportID = $this->ReadPropertyInteger('Export');
             $export = 1; // Standardwert setzen
@@ -244,9 +247,10 @@ class TileVisuPVoverview extends IPSModule
             $export_prozent = round($export / $produktion * 100, 0);
             $eigenverbrauch_prozent = round(100 - $export_prozent, 0);
             $eigenverbrauch = round($produktion / 100 * $eigenverbrauch_prozent,2);
-            $import_prozent = round($import / $verbrauch * 100, 0);
             $eigenproduktion_prozent = round(100 - $import_prozent, 0);
             $eigenproduktion = round($produktion - $export, 2);
+            $verbrauch = $import + $eigenproduktion;
+            $import_prozent = round($import / $verbrauch * 100, 0);
 
             
             $result['produktion'] = $produktion;
