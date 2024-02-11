af14e5502afaa927ac5c2f3729d642f6867b60b3
da8ter <58776097+da8ter@users.noreply.github.com>
2024-02-11 16:01:06 +0100

    sd

--- PV_Overview/module.php
+++ PV_Overview/module.php
@@ -325,6 +325,58 @@ class TileVisuPVoverview extends IPSModule
             $result['eigenproduktion'] =  $eigenproduktion;
 
 
+
+            $imageID = $this->ReadPropertyInteger('bgImage');
+            if (IPS_MediaExists($imageID)) {
+                $image = IPS_GetMedia($imageID);
+                if ($image['MediaType'] === MEDIATYPE_IMAGE) {
+                    $imageFile = explode('.', $image['MediaFile']);
+                    $imageContent = '';
+                    // Falls ja, ermittle den Anfang der src basierend auf dem Dateitypen
+                    switch (end($imageFile)) {
+                        case 'bmp':
+                            $imageContent = 'data:image/bmp;base64,';
+                            break;
+    
+                        case 'jpg':
+                        case 'jpeg':
+                            $imageContent = 'data:image/jpeg;base64,';
+                            break;
+    
+                        case 'gif':
+                            $imageContent = 'data:image/gif;base64,';
+                            break;
+    
+                        case 'png':
+                            $imageContent = 'data:image/png;base64,';
+                            break;
+    
+                        case 'ico':
+                            $imageContent = 'data:image/x-icon;base64,';
+                            break;
+                    }
+
+                    // Nur fortfahren, falls Inhalt gesetzt wurde. Ansonsten ist das Bild kein unterstützter Dateityp
+                    if ($imageContent) {
+                        // Hänge base64-codierten Inhalt des Bildes an
+                        $imageContent .= IPS_GetMediaContent($imageID);
+                        $result['image1'] = $imageContent;
+                    }
+
+                }
+            }
+            else{
+                $imageContent = 'data:image/png;base64,';
+                $imageContent .= base64_encode(file_get_contents(__DIR__ . '/assets/placeholder.png'));
+                $result['image1'] = $imageContent;
+            }     
+
+
+
+
+
+
+
         return json_encode($result);
     }
 
