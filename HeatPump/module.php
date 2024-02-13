1d1fa529318a16d0a0615bcce9f530080889df21
da8ter <58776097+da8ter@users.noreply.github.com>
2024-02-13 13:43:59 +0100

    sd

--- HeatPump/module.php
+++ HeatPump/module.php
@@ -50,7 +50,7 @@
             $this->RegisterPropertyInteger('luefterbalkenfarbe2', 0x23B3FB);
             $this->RegisterPropertyInteger("bgImage", 0);
             $this->RegisterPropertyFloat("Bildtransparenz", 0.7);
-            $this->RegisterPropertyInteger("Kachelhintergrundfarbe", -1);
+            $this->RegisterPropertyInteger("Kachelhintergrundfarbe", 0x000000);
             $this->RegisterPropertyBoolean("BG_Off", 1);
             $this->SetVisualizationType(1);
         }
