2e8d7b9cc2af0ed1e4b26992d74c39c5d76798a5
da8ter <58776097+da8ter@users.noreply.github.com>
2024-02-15 20:09:27 +0100

    sd

--- WashingMachineDryer/module.php
+++ WashingMachineDryer/module.php
@@ -319,7 +319,7 @@ class TileVisuWashingMaschine extends IPSModule
             }
             else{
                 $imageContent = 'data:image/png;base64,';
-                $imageContent .= base64_encode(file_get_contents(__DIR__ . '/ ../assets/placeholder.png'));
+                $imageContent .= base64_encode(file_get_contents(__DIR__ . '/../assets/placeholder.png'));
 
                 if ($this->ReadPropertyBoolean('BG_Off')) {
                     $result['image1'] = $imageContent;
