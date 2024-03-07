ad2ef23370d7805f0c98c3474221d7a4a0d300b9
da8ter <58776097+da8ter@users.noreply.github.com>
2024-03-07 16:05:01 +0100

    df

--- PV_Overview/module.php
+++ PV_Overview/module.php
@@ -312,8 +312,8 @@ class TileVisuPVOverview extends IPSModule
 
             // Eingabewerte
             $produktion = 1000; // in kWh
-            $beladungSpeicher = 200; // in kWh
-            $entladungSpeicher = 90; // in kWh
+            $beladungSpeicher = 100; // in kWh
+            $entladungSpeicher = 100; // in kWh
             $import = 510; // in kWh
             $export = 100; // in kWh
             $verbrauch = 1500;
