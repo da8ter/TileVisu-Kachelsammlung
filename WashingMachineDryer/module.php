2b82b26b0077dd09defd5d6e4758a9e1e53b1e97
da8ter <58776097+da8ter@users.noreply.github.com>
2024-03-18 09:05:41 +0100

    er

--- WashingMachineDryer/module.php
+++ WashingMachineDryer/module.php
@@ -40,6 +40,16 @@ class TileVisuWashingMaschine extends IPSModule
     {
         parent::ApplyChanges();
 
+
+        $refs = $this->GetReferenceList();
+            foreach($refs as $ref) {
+                $this->UnregisterReference($ref);
+            }
+            $ids = IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}");
+            foreach($ids as $id) {
+                $this->RegisterReference($id);
+            }
+
         // Aktualisiere registrierte Nachrichten
         foreach ($this->GetMessageList() as $senderID => $messageIDs)
         {
