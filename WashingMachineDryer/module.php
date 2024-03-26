3db4bc48972173e806bb59b539726e758124c1a3
da8ter <58776097+da8ter@users.noreply.github.com>
2024-03-26 11:06:58 +0100

    sd

--- WashingMachineDryer/module.php
+++ WashingMachineDryer/module.php
@@ -107,7 +107,7 @@ class TileVisuWashingMaschine extends IPSModule
                                 $restlaufzeitInSeconds = $hours * 3600 + $minutes * 60 + $seconds;
                                 
                                 // Aktualisiere die Visualisierung oder verarbeite den Wert weiter, falls nötig
-                                $this->UpdateVisualizationValue(json_encode(['RestlaufzeitInSeconds' => $restlaufzeitInSeconds]));
+                                $this->UpdateVisualizationValue(json_encode(['Restlaufzeit' => $restlaufzeitInSeconds]));
                             }
                         }
                         else {
