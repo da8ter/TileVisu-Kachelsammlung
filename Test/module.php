d51e59b1ad3b580768ee3824a5e576ec9a3d53b0
da8ter <58776097+da8ter@users.noreply.github.com>
2024-04-03 12:07:51 +0200

    sd

--- Test/module.php
+++ Test/module.php
@@ -8,7 +8,6 @@
             parent::Create();
 
             // Drei Eigenschaften für die dargestellten Zähler
-            $this->RegisterPropertyInteger("bgImage", 0);
             // Visualisierungstyp auf 1 setzen, da wir HTML anbieten möchten
             $this->SetVisualizationType(1);
         }
@@ -16,19 +15,7 @@
         public function ApplyChanges() {
             parent::ApplyChanges();
 
-                                //Referenzen Registrieren
-        $ids = [
-            $this->ReadPropertyInteger('bgImage')
-        ];
-        $refs = $this->GetReferenceList();
-            foreach($refs as $ref) {
-                $this->UnregisterReference($ref);
-            } 
-            foreach ($ids as $id) {
-                if ($id !== '') {
-                    $this->RegisterReference($id);
-                }
-            }
+      
 
         }
 
@@ -51,51 +38,7 @@
             private function GetFullUpdateMessage() {
                 $result = []; // Initialisiere das Ergebnis-Array
 
-
-                    $imageID = $this->ReadPropertyInteger('bgImage');
-                    if (IPS_MediaExists($imageID)) {
-                        $image = IPS_GetMedia($imageID);
-                        if ($image['MediaType'] === MEDIATYPE_IMAGE) {
-                            $imageFile = explode('.', $image['MediaFile']);
-                            $imageContent = '';
-                            // Falls ja, ermittle den Anfang der src basierend auf dem Dateitypen
-                            switch (end($imageFile)) {
-                                case 'bmp':
-                                    $imageContent = 'data:image/bmp;base64,';
-                                    break;
-            
-                                case 'jpg':
-                                case 'jpeg':
-                                    $imageContent = 'data:image/jpeg;base64,';
-                                    break;
-            
-                                case 'gif':
-                                    $imageContent = 'data:image/gif;base64,';
-                                    break;
-            
-                                case 'png':
-                                    $imageContent = 'data:image/png;base64,';
-                                    break;
-            
-                                case 'ico':
-                                    $imageContent = 'data:image/x-icon;base64,';
-                                    break;
-                            }
-
-                            // Nur fortfahren, falls Inhalt gesetzt wurde. Ansonsten ist das Bild kein unterstützter Dateityp
-                            if ($imageContent) {
-                                // Hänge base64-codierten Inhalt des Bildes an
-                                $imageContent .= IPS_GetMediaContent($imageID);
-                                $result['image1'] = $imageContent;
-                            }
-
-                        }
-                    }
-                    else{
-                        $imageContent = 'data:image/png;base64,';
-                        $imageContent .= base64_encode(file_get_contents(__DIR__ . '/assets/placeholder.png'));
-                        $result['image1'] = $imageContent;
-                    }                    
+                                
                     
                 
                 
